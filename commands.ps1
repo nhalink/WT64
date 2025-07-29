5049859538ea91453454142af252af1f5e9e2418
Niko Halink <nhalink@gmail.com>
2025-07-29 22:38:09 +0200

    Code cleanup

--- commands.ps1
+++ commands.ps1
@@ -1,122 +1,157 @@
 $e = "$([char]27)"
 
-function CenterText([string]$inputLine,[int]$textWidth,[string]$wrapChar) {
-    If (($inputLine.length + ($wrapChar.Length *2)) -ge $textWidth) {
-
-        return -join($wrapChar,$inputLine.Substring(0,$textWidth-2*$wrapChar.Length),$wrapChar)}
-    else {
-        $diff =  ($textWidth+(2*$wrapChar.Length)) - $inputLine.Length
-        $spaceCount = [math]::Floor($diff/2)
+function CenterText([string]$inputLine, [int]$textWidth, [string]$wrapChar)
+{
+    If (($inputLine.length + ($wrapChar.Length * 2)) -ge $textWidth)
+    {
+        return -join($wrapChar, $inputLine.Substring(0, $textWidth - 2 * $wrapChar.Length), $wrapChar)
+    }
+    else
+    {
+        $diff = ($textWidth + (2 * $wrapChar.Length)) - $inputLine.Length
+        $spaceCount = [math]::Floor($diff / 2)
         $leadingSpaces = $(" " * $spaceCount)
-        if (-join( $wrapChar, $leadingSpaces, $inputLine, $leadingSpaces,$wrapChar).Length -lt $textWidth) {
-            return -join( $wrapChar, $leadingSpaces, $inputLine, $leadingSpaces,$wrapChar, " ")
-        } else {
-            return -join( $wrapChar, $leadingSpaces, $inputLine, $leadingSpaces,$wrapChar)
-            
+
+        if (-join( $wrapChar, $leadingSpaces, $inputLine, $leadingSpaces, $wrapChar).Length -lt $textWidth)
+        {
+            return -join( $wrapChar, $leadingSpaces, $inputLine, $leadingSpaces, $wrapChar, " ")
+        }
+        else
+        {
+            return -join( $wrapChar, $leadingSpaces, $inputLine, $leadingSpaces, $wrapChar)
         }
     }
 }
-function LeftText([string]$inputLine,[int]$textWidth,[string]$wrapChar) {
-    If ($inputLine.length -ge $textWidth) {
-        return -join($inputLine.Substring(0,$textWidth))
-    }
-    else {
-        $diff =  $textWidth- $inputLine.Length
-        $leadingSpaces = $(" " * $diff)
-        return -join($inputLine,$leadingSpaces)
-    }
 
+function LeftText([string]$inputLine, [int]$textWidth, [string]$wrapChar)
+{
+    If ($inputLine.length -ge $textWidth)
+    {
+        return -join($inputLine.Substring(0, $textWidth))
+    }
+    else
+    {
+        $diff = $textWidth - $inputLine.Length
+        $leadingSpaces = $(" " * $diff)
+
+        return -join($inputLine, $leadingSpaces)
+    }
 }
 
-function RightText([string]$inputLine,[int]$textWidth,[string]$wrapChar) {
-    If (($inputLine.length+(2*$wrapChar.Length)) -ge $textWidth-1) {
-        $trimmed = $textWidth - (2*$wrapChar.Length)
-        return -join($wrapChar,$inputLine.Substring(0, $trimmed-2),$wrapChar,"  ")
-    }
-    else {
-        $diff =  $textWidth - ($inputLine.Length+(2*$wrapChar.Length))
+function RightText([string]$inputLine, [int]$textWidth, [string]$wrapChar)
+{
+    If (($inputLine.length + (2 * $wrapChar.Length)) -ge $textWidth - 1)
+    {
+        $trimmed = $textWidth - (2 * $wrapChar.Length)
 
+        return -join($wrapChar, $inputLine.Substring(0, $trimmed - 2), $wrapChar, "  ")
+    }
+    else
+    {
+        $diff = $textWidth - ($inputLine.Length + (2 * $wrapChar.Length))
         $leadingSpaces = $(" " * $diff)
+
         return -join($wrapChar, $inputLine, $wrapChar, $leadingSpaces)
     }
-
 }
-function LIST ([string]$dirName){
+
+function LIST([string]$dirName)
+{
     $midWidth = [int] (Get-Host).UI.RawUI.MaxWindowSize.Width - 4
-    if ($dirName.Length -gt 0) {
+
+    if ($dirName.Length -gt 0)
+    {
         $folderName = [System.IO.Path]::GetDirectoryName($dirName).toUpper().split("\")[-1]
-    } else {
+    }
+    else
+    {
         $folderName = (Get-Location).toString().toUpper().split("\")[-1]
     }
-        $e = "$([char]27)"
-    -join("0 $e[44m$e[94m$e[7m",(CenterText $folderName ($midWidth-6)),"$e[27m$e[0m") 
-Get-ChildItem $dirName| ForEach-Object { -join((LeftText ([math]::Round($_.Length/100)).ToString() 6), ' ', (RightText $_.name.toUpper().Split(".")[0] ($midWidth-14) '"')," ", (RightText $_.name.toUpper().Split(".")[1] 10 ))}
--join((Get-PSDrive c).Free, " BLOCKS FREE.")
-"READY."
+
+    $e = "$([char]27)"
+    -join("0 $e[44m$e[94m$e[7m", (CenterText $folderName ($midWidth - 6)), "$e[27m$e[0m")
+    Get-ChildItem $dirName | ForEach-Object { -join((LeftText ([math]::Round($_.Length/100)).ToString() 6), ' ', (RightText $_.name.toUpper().Split(".")[0] ($midWidth - 14) '"'), " ", (RightText $_.name.toUpper().Split(".")[1] 10)) }
+    -join((Get-PSDrive c).Free, " BLOCKS FREE.")
+    "READY."
 }
-function SYS64738() {
-"$e[0m"
+
+function SYS64738()
+{
+    "$e[0m"
     Clear-Host
-""
-$line1 = "**** WINDOWS TERMINAL POWERSHELL V" + $PSVersionTable.PSVersion.Major + "." + $PSVersionTable.PSVersion.Minor + " ****"
-$line2 = " " + $mem + "K SYSTEM RAM "+ $free  +"000 BASIC BYTES FREE"
+    ""
+    $line1 = "**** WINDOWS TERMINAL POWERSHELL V" + $PSVersionTable.PSVersion.Major + "." + $PSVersionTable.PSVersion.Minor + " ****"
+    $line2 = " " + $mem + "K SYSTEM RAM " + $free + "000 BASIC BYTES FREE"
 
-CenterText $line1 ((Get-Host).UI.RawUI.MaxWindowSize.Width)
-" "
-CenterText $line2 ((Get-Host).UI.RawUI.MaxWindowSize.Width)
-" "
-"READY."
+    CenterText $line1 ((Get-Host).UI.RawUI.MaxWindowSize.Width)
+    " "
+    CenterText $line2 ((Get-Host).UI.RawUI.MaxWindowSize.Width)
+    " "
+    "READY."
 }
 
-function LOAD([string]$inputLn) {
-    if ($inputLn -eq '$ 8') {
+function LOAD([string]$inputLn)
+{
+    if ($inputLn -eq '$ 8')
+    {
         "SEARCHING FOR $"
         Start-Sleep -Seconds 2
         "LOADING"
         Start-Sleep -Seconds 1
         "READY."
-        
-    } else {
+    }
+    else
+    {
         "SYNTAX ERROR"
     }
 }
-function EDIT ($File){
-$File = $File -replace “\\”, “/” -replace “ “, “\ “
-bash -c "nano $File"
-}
 
+function EDIT($File)
+{
+    $File = $File -replace “\\”, “/” -replace “ “, “\ “
+    bash -c "nano $File"
+}
 
 #
 # Internal commands (should be hidden inside of a module)
 #
 
-$varNameRegEx='([a-zA-Z]+)([\$|\%]?)' # returns name and type
-$expressionRegEx='\s*(\S+)\s*'        # returns expression
+$varNameRegEx = '([a-zA-Z]+)([\$|\%]?)' # returns name and type
+$expressionRegEx = '\s*(\S+)\s*'        # returns expression
 
-function _CalculateExpressionValue([string]$Expression) {
+function _CalculateExpressionValue([string]$Expression)
+{
     # TODO: Hide this in module
-    while ($Expression -match $varNameRegEx) {
+    while ($Expression -match $varNameRegEx)
+    {
         $Expression = $Expression -replace $Matches[0], [string](Get-Variable -Name $Matches[1] -ValueOnly -Scope Global)
     }
-    try {
+
+    try
+    {
         return (Invoke-Expression $Expression)
-    } catch {
+    }
+    catch
+    {
         "SYNTAX ERROR"
     }
 }
-
 
 #
 # Public functions = C64 commands
 #
 
-function RUN ($File){
+function RUN($File)
+{
     Invoke-Item $File
 }
 
-function  LET([string]$inputLn)  {
+function LET([string]$inputLn)
+{
     $regex = "^\s*$varNameRegEx\s*=\s*$expressionRegEx\s*"
-    if ($inputLn -notmatch $regex) {
+
+    if ($inputLn -notmatch $regex)
+    {
         "SYNTAX ERROR"
         exit
     }
@@ -129,6 +164,7 @@ function  LET([string]$inputLn)  {
     # TODO: Include Type also
 }
 
-function  PRINT([string]$inputLn) {
+function PRINT([string]$inputLn)
+{
     Write-Host (_CalculateExpressionValue $inputLn)
 }
--- reset.ps1
+++ reset.ps1
@@ -1,31 +1,36 @@
 "LOADING PLEASE WAIT ..."
 $e = "$([char]27)"
-$mem = [regex]::Match((wmic OS get TotalVisibleMemorySize /Value),'[0-9]+').Value + ''
-$free = [regex]::Match((wmic OS get FreePhysicalMemory /Value),'[0-9]+').Value + ''
+$mem = [regex]::Match((wmic OS get TotalVisibleMemorySize /Value), '[0-9]+').Value + ''
+$free = [regex]::Match((wmic OS get FreePhysicalMemory /Value), '[0-9]+').Value + ''
 
 # This will display READY.\n as prompt
 # function prompt {"READY.`n"}
 function prompt {"`r"}
-function CenterText([string]$inputLine,[int]$textWidth,[string]$wrapChar) {
-    If (($inputLine.length + ($wrapChar.Length *2)) -ge $textWidth) {
 
-        return -join($wrapChar,$inputLine.Substring(0,$textWidth-2*$wrapChar.Length),$wrapChar)}
-    else {
-        $diff =  ($textWidth+(2*$wrapChar.Length)) - $inputLine.Length
-        $spaceCount = [math]::Floor($diff/2)
+function CenterText([string]$inputLine, [int]$textWidth, [string]$wrapChar)
+{
+    If (($inputLine.length + ($wrapChar.Length * 2)) -ge $textWidth)
+    {
+        return -join($wrapChar, $inputLine.Substring(0, $textWidth - 2 * $wrapChar.Length), $wrapChar)
+    }
+    else
+    {
+        $diff = ($textWidth + (2 * $wrapChar.Length)) - $inputLine.Length
+        $spaceCount = [math]::Floor($diff / 2)
         $leadingSpaces = $(" " * $spaceCount)
-        if (-join( $wrapChar, $leadingSpaces, $inputLine, $leadingSpaces,$wrapChar).Length -lt $textWidth) {
-            return -join( $wrapChar, $leadingSpaces, $inputLine, $leadingSpaces,$wrapChar, " ")
-            
-        } else {
-            return -join( $wrapChar, $leadingSpaces, $inputLine, $leadingSpaces,$wrapChar)
-            
+
+        if (-join( $wrapChar, $leadingSpaces, $inputLine, $leadingSpaces, $wrapChar).Length -lt $textWidth)
+        {
+            return -join( $wrapChar, $leadingSpaces, $inputLine, $leadingSpaces, $wrapChar, " ")
+        }
+        else
+        {
+            return -join( $wrapChar, $leadingSpaces, $inputLine, $leadingSpaces, $wrapChar)
         }
     }
-
 }
+
 $scriptRoot = Split-Path -parent $PSCommandPath
 . "$scriptRoot\commands.ps1"
 
 SYS64738
-
